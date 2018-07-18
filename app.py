e10b8db11ef97a3ee94452e787dddc6f87c29f7c
aquawill <aquawill@msn.com>
2018-07-18 15:24:34 +0800

    commit

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python
-import io
-from urllib.request import Request, urlopen
 import os
+import random
+from urllib.request import Request, urlopen
+
 import cv2
 import numpy as np
 import tensorflow as tf
 from flask import Flask
-from flask_restful import reqparse, abort, Api, Resource
-import random
+from flask_restful import reqparse, Api, Resource
+import ssl
 
 from image_utils import create_tensorflow_image_loader
 from image_utils import create_yahoo_image_loader
 from model import OpenNsfwModel, InputType
 
-
 IMAGE_LOADER_TENSORFLOW = "tensorflow"
 IMAGE_LOADER_YAHOO = "yahoo"
+context = ssl._create_unverified_context()
 
-def classifier(img):
+def classifier(img, url):
     image_loader = 'yahoo'
     input_file = img
     input_type = 'tensor'
@@ -26,7 +27,6 @@ def classifier(img):
     model = OpenNsfwModel()
 
     with tf.Session() as sess:
-        tf.get_variable_scope().reuse_variables()
         input_type = InputType[input_type.upper()]
         model.build(weights_path=model_weights, input_type=input_type)
 
@@ -40,32 +40,37 @@ def classifier(img):
         elif input_type == InputType.BASE64_JPEG:
             import base64
             fn_load_image = lambda filename: np.array([base64.urlsafe_b64encode(open(filename, "rb").read())])
-            #fn_load_image = img
+            # fn_load_image = img
 
         sess.run(tf.global_variables_initializer())
         image = fn_load_image(input_file)
         predictions = sess.run(model.predictions, feed_dict={model.input: image})
-    return {'sfw': str(predictions[0][0]), 'nsfw': str(predictions[0][1])}
+    return {'url': url, 'sfw': str(predictions[0][0]), 'nsfw': str(predictions[0][1])}
+
 
 app = Flask(__name__)
+
 
 def dl_img(img_url):
     req = Request(img_url)
     req.add_header("User-Agent",
                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36")
-    img = urlopen(req)
+    img = urlopen(req, context=context)
     arr = np.asarray(bytearray(img.read()), dtype=np.uint8)
     return arr
+
 
 class nsfw_image_api(Resource):
     def post(self):
         args = parser.parse_args()
-        url = args['url']
+        print(args)
+        url = args.get('url')
+        urls = args.get('urls')
         img_arr = dl_img(url)
         im = cv2.imdecode(img_arr, -1)
         file_name = random.randint(0, 9999999999999999)
         cv2.imwrite('{}.jpg'.format(str(file_name)), im)
-        result = classifier('{}.jpg'.format(str(file_name)))
+        result = classifier('{}.jpg'.format(str(file_name)), url)
         os.remove('{}.jpg'.format(str(file_name)))
         return result
 
